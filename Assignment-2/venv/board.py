678ec7442d545b327f0662e756cec54573dbf539
Justin Terry <Justin.Terry@student.csulb.edu>
2019-09-08 19:53:00 -0700

    Genetic algorithm usually finds an result

--- Assignment-2/venv/board.py
+++ Assignment-2/venv/board.py
@@ -6,17 +6,45 @@ class Board:
     def __init__(self, n):
         self.n_queen = n
         self.map = [[0 for j in range(n)] for i in range(n)]
+        self.fit = n * (n-1) // 2
+
 
     def set_queens(self):
         for i in range(self.n_queen):
             j = random.randint(0, self.n_queen - 1)
             self.map[i][j] = 1
 
+    def set_state(self, state):
+        self.map = [[0 for j in range(self.n_queen)] for i in range(self.n_queen)]
+        for i in range(0, self.n_queen):
+            self.map[i][int(state[i])] = 1;
+
+    def get_state(self):
+        state = ""
+        for r in range(0, 5):
+            for c in range(0, 5):
+                if self.map[r][c] == 1:
+                    state += (str(c))
+        return state
+
     def show(self):
         print(np.matrix(self.map))
 
     def getMap(self):
         return self.map
+
+    def genetic_fitness(self):
+        self.fit = self.n_queen * (self.n_queen - 1) // 2
+        for i in range(self.n_queen):
+            for j in range(self.n_queen):
+                if self.map[i][j] == 1:
+                    for k in range(1, self.n_queen - i):
+                        if self.map[i + k][j] == 1:
+                            self.fit -= 1
+                        if j - k >= 0 and self.map[i + k][j - k] == 1:
+                            self.fit -= 1
+                        if j + k < self.n_queen and self.map[i + k][j + k] == 1:
+                            self.fit -= 1
 
 
 if __name__ == '__main__':
--- Assignment-2/venv/genetic.py
+++ Assignment-2/venv/genetic.py
@@ -1,77 +1,125 @@
 from board import Board
-
-# Maximum non-attacking pairs is 30
+import random
+# Maximum non-attacking pairs is 10
 
 class Genetic:
     def __init__(self):
-        self.no_steps = 0
-        self.board = Board(5)
-        self.board.set_queens()
-        self.board.show()
-        self.calculateH()
+        self.no_boards = 10
+        self.no_steps = 1
+        self.boards = []
+        self.create_boards()
+
+        while(True):
+            print("Step #" + str(self.no_steps))
+            self.show_boards()
+            self.show_fitness()
+            self.show_states()
+            self.perform_swap()
+            self.no_steps += 1
+            print("\n\n")
         exit()
 
-    # Returns the state in format x1 y1 x2 y2 x3 y3 etc with no spaces
-    def getState(self):
-        state = ""
-        for r in range(0, 5):
-            for c in range(0, 5):
-                if self.board.getMap()[r][c] == 1:
-                    state += (str(c))
-        return state
+    def create_boards(self):
+        for i in range(0, self.no_boards):
+            newBoard = Board(5)
+            newBoard.set_queens()
+            self.boards.insert(i, newBoard)
 
-    def calculateH(self):
-        state = self.getState()
-        totalAttackingPairs = 0
-        for i in range(0,5):
-            totalAttackingPairs += self.checkDiagnol(int(i), "03142")
-            totalAttackingPairs += self.checkColumn(int(i), "03142")
-        print(totalAttackingPairs)
+    def show_boards(self):
+        for i in range(0, 5):
+            for board in self.boards:
+                print(str(board.getMap()[i]).replace(",", ""), end="    ")
+            print()
 
-    # Checks all the columns for the row, r, given
-    def checkColumn(self, r, state):
-        row = int(r)
-        rowValue =int(state[row])
-        nonAttacking = 0
-        conflicts = 0
-        for currRow in range(0, len(state)):
-            if(currRow != row):
-                if(int(state[currRow]) != rowValue):
-                    nonAttacking+=1
-                else:
-                    conflicts += 1
-        print("Column Conflicts: ", conflicts)
-        return nonAttacking
+    def show_fitness(self):
+        for board in self.boards:
+            board.genetic_fitness()
+            print("Fitness:",board.fit, "    ", end="")
+        print()
 
-    def checkDiagnol(self, r, state):
-        print("Checking row: ", r)
-        row = int(r)
-        rowValue = int(state[row])
-        conflicts = 0
-        nonAttacking = 0
-        tempRow = int(row - 1);
-        tempColumn = int(rowValue - 1)
-        while(tempRow >= 0 and tempColumn >= 0):
-            print("Against: row: ", tempRow, " column: ", tempColumn)
-            if(int(state[tempRow]) == tempColumn):
-                print(tempRow, " has a diag conflict")
-                conflicts+=1
-            tempRow-=1
-            tempColumn-=1
-        tempRow = int(row + 1)
-        tempColumn = int(rowValue + 1)
-        while(tempRow < 5 and tempColumn < 5):
-            print("Against: row: ", tempRow, " column: ", tempColumn)
-            if(int(state[tempRow]) == tempColumn):
-                print(tempRow, " has a diag conflict")
-                conflicts+=1
-            tempRow+=1
-            tempColumn+=1
-        if(conflicts == 0):
-            nonAttacking += 2
+    def show_states(self):
+        for board in self.boards:
+            print("State:",board.get_state(),"  ", end="")
+        print()
 
-        print("Diag Conflicts: ", conflicts)
-        return nonAttacking
+    def perform_swap(self):
+        fitness_values, fitness_total = self.get_fitness_values()
+
+        fitness_values = self.map_fitness_to_percent(fitness_values, fitness_total)
+
+        new_states = self.generate_selection(fitness_values)
+
+        options = random.sample(range(0,self.no_boards), self.no_boards)
+        splits = []
+        for i in range(0, int(self.no_boards/2)):
+            splits.insert(i, random.uniform(0, int(self.no_boards/2)))
+
+        mutated_states = self.perform_crossover(new_states, options, splits)
+
+        for i in range(0, len(self.boards)):
+            self.boards[i].set_state(mutated_states[i])
+
+    def finished(self):
+        print("Finished in", self.no_steps, "steps.")
+
+    def perform_crossover(self, states, options, splits):
+        print(options)
+        split = 0
+        for i in range(0,len(states),2):
+            state1 = states[options[i]]
+            state2 = states[options[i+1]]
+            states[options[i]] = state1[0:int(splits[split])] + state2[int(splits[split]):len(state2)]
+            states[options[i+1]] = state2[0:int(splits[split])] + state1[int(splits[split]):int(len(state2))]
+            split+=1
+
+        indexes = random.sample(range(0,self.no_boards-1), self.no_boards-1)
+        print("Indexes:", indexes)
+        print("States:", states)
+        for i in range(0, self.no_boards-1):
+            s = states[indexes[i]]
+            print("State:", s, "Index:", indexes[i])
+            char = str(random.randint(0,4))
+            print("c:",char)
+            s = s[0:indexes[i]] + char + s[indexes[i]+1:]
+            states[indexes[i]] = s
+            print("New State:", s)
+
+        return states
+
+    def generate_selection(self, fitness_values):
+        new_states = []
+        maxIndex = fitness_values.index(max(fitness_values))
+        fitness_values[maxIndex] = fitness_values[maxIndex]*100
+        for i in range(0,self.no_boards):
+            selector = random.uniform(0,fitness_values[maxIndex])
+            if selector > 0 and selector < fitness_values[0]:
+                new_states.insert(i, self.boards[0].get_state())
+            elif selector > fitness_values[0] and selector < fitness_values[0] + fitness_values[1]:
+                new_states.insert(i, self.boards[1].get_state())
+            elif selector > fitness_values[0] + fitness_values[1] and selector < fitness_values[0] + fitness_values[1] + fitness_values[2]:
+                new_states.insert(i, self.boards[2].get_state())
+            else:
+                new_states.insert(i, self.boards[3].get_state())
+        return new_states
+
+    def get_fitness_values(self):
+        fitness_values = []
+        fitness_total = 0
+
+        for i in range(0, self.no_boards):
+            fitness_values.insert(i, self.boards[i].fit)
+            fitness_total += self.boards[i].fit
+            if(fitness_values[i] == 10):
+                self.finished()
+                exit();
+        return fitness_values, fitness_total
+
+    def map_fitness_to_percent(self, fitness_values, fitness_total):
+        for i in range(0, self.no_boards):
+            temp = fitness_values[i]
+            fitness_values[i] = temp/fitness_total
+        return fitness_values
+
 
 # if __name__ == '__main__':
 #     brd = Board(5)
