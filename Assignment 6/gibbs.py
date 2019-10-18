d4ee260cebd4abc6ba31d2354dcb788db5a4ace7
Justin Terry <Justin.Terry@student.csulb.edu>
2019-10-18 16:13:24 -0700

    Setup Assignment 6

--- Assignment 6/gibbs.py
+++ Assignment 6/gibbs.py
@@ -1,22 +1,23 @@
+from tabulate import tabulate
+
 class Gibbs:
 
     def __init__(self):
-        self.cloudy = 0.5
-        # prob of sprinkler given C=[t, f]
-        self.sprinkler = [0.1, 0.5]
-        # prob of rain given C=[t, f]
-        self.rain = [0.8, 0.2]
-        # prob of wet grass given
-        self.wet_grass = [0.99, 0.9, 0.9, 0]
+        self.transition = [["S1", .34485,     .5,     .15515,     0],
+                           ["S2", 0,          .5626,  0,          .4374],
+                           ["S3", .34485,     0,      .15515,     .5],
+                           ["S4", 0,          .0626,  0,          .9374]]
+        self.partA()
+        self.partB()
 
+    def partA(self):
+        print("P(C|-s,r) = <0.8748, 0.1252>")
+        print("P(C|-s,-r) = <0.3103, 0.6897>")
+        print("P(R|c,-s,w) = <1,0>")
+        print("P(R|-c,-s,w) = <1,0>")
 
-
-
-
-
-
-
-
+    def partB(self):
+        print(tabulate(self.transition, ["S1", "S2", "S3", "S4"], tablefmt="simple"))
 
 if __name__ == '__main__':
     driver = Gibbs()
