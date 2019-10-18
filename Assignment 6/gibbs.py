4dcb9bc93f38db79d6c008d1fb4bcf0e0cdb97f6
Justin Terry <Justin.Terry@student.csulb.edu>
2019-10-18 16:56:56 -0700

    Finished up assignment 6

--- Assignment 6/gibbs.py
+++ Assignment 6/gibbs.py
@@ -1,4 +1,6 @@
+#tabulate is used for outputing partB
 from tabulate import tabulate
+import random
 
 class Gibbs:
 
@@ -9,6 +11,7 @@ class Gibbs:
                            ["S4", 0,          .0626,  0,          .9374]]
         self.partA()
         self.partB()
+        self.performMCMC()
 
     def partA(self):
         print("P(C|-s,r) = <0.8748, 0.1252>")
@@ -18,6 +21,83 @@ class Gibbs:
 
     def partB(self):
         print(tabulate(self.transition, ["S1", "S2", "S3", "S4"], tablefmt="simple"))
+
+    def createRandomState(self):
+        state = []
+        c = random.randint(0,1)
+        state.append(c)
+        state.append(self.generateRandomR(c))
+        state.append(self.generateRandomS(c))
+        state.append(self.generateRandomW(state[1], state[2]))
+        return state
+
+    def generateRandomR(self, C):
+        ran = random.random()
+        if(C == 1):
+            if(ran >= .2):
+                return 1
+            else:
+                return 0
+        else:
+            if(ran >= .8):
+                return 1
+            else:
+                return 0
+
+    def generateRandomS(self, C):
+        ran = random.random()
+        if(C == 1):
+            if(ran >= .9):
+                return 1
+            else:
+                return 0
+        else:
+            if(ran >= .5):
+                return 1
+            else:
+                return 0
+
+    def generateRandomW(self, R, S):
+        ran = random.random()
+        if(R == 1 and S == 1):
+            #r,s
+            if(ran >= .99):
+                return 0
+            else:
+                return 1
+        if(R == 1 and S == 0):
+            #r, -s
+            if(ran >= .9):
+                return 0
+            else:
+                return 1
+        if(R == 0 and S == 1):
+            #-r, s
+            if(ran >= .9):
+                return 0
+            else:
+                return 1
+        if(R == 0 and S == 0):
+            #-r, -s
+            return 0
+
+
+
+    def performMCMC(self):
+        numOfNotC = 0
+        numOfC = 0
+        numOfRuns = 0
+        for i in range(1000000):
+            state = self.createRandomState()
+            if(state[0] == 0 and state[2] == 0 and state[3] == 1):
+                numOfNotC += 1
+                numOfRuns += 1
+            elif (state[0] == 1 and state[2] == 0 and state[3] == 1):
+                numOfC += 1
+                numOfRuns += 1
+
+        print("<", numOfC/numOfRuns, ",", numOfNotC/numOfRuns, ">")
+
 
 if __name__ == '__main__':
     driver = Gibbs()
